addbe7da97f34e17ac8387620170e4a342e7059d
adharikrishnan <adharikrishnan456@gmail.com>
2023-02-21 18:48:08 +0530

    added main functionality

--- app.js
+++ app.js
@@ -9,19 +9,34 @@ function cacculateData(e){
     const eYears = document.getElementById("years");
 
     const eMonthlyPayment = document.getElementById("monthly-payment");
-    const eTotalPayment = document.getElementById("monthly-payment");
+    const eTotalPayment = document.getElementById("total-payment");
     const eTotalInterest = document.getElementById("total-interest")
 
 
     const principal = parseFloat(eAmount.value);
-    const interestRate = parseFloat(eInterest.value) / 100 / 12;
-    const cacculatePayment = parseFloat(eYears.value) * 12
+    const monthlyInterest = parseFloat(eInterest.value) / 100 / 12;
+    const monthsToPayback = parseFloat(eYears.value) * 12;
 
     // Calculate monthly payments
-    const 
-
-
+    const cRate = Math.pow( 1 + monthlyInterest, monthsToPayback);
+    const monthlyPayment = (principal * monthlyInterest * cRate) / (cRate - 1)
+    
+    if(isFinite(monthlyPayment)){
+        eMonthlyPayment.value = monthlyPayment.toFixed(2);
+        eTotalPayment.value = (monthlyPayment * monthsToPayback).toFixed(2);
+        eTotalInterest.value = ((monthlyPayment * monthsToPayback) - principal).toFixed(2);
+    } else {
+        showError("Something Wrong with the values entered!");
+    }
 
     e.preventDefault();
+}
+
+function showError(error){
+
+    const errorDiv = document.createElement('div');
+
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error))
 }
 
